5f448ea173404614be413d09d32b3ca5ea573324
Dheer Joisher <dheerjoisher7@gmail.com>
2024-05-14 21:39:22 +0530

    Updates

--- src/Components/ForgotPassword/ForgotPassword.js
+++ src/Components/ForgotPassword/ForgotPassword.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import './ForgotPassword.css'
-import img from './img.png'
+import React from 'react';
+import styles from './ForgotPassword.module.css'; // Import the CSS module
+import img from './img.png';
 import { Link } from 'react-router-dom';
 
 export default function MainPage() {
   return (
-    
-    <div style={{height : '100vh' , padddingBottom : '50px' , paddingTop : '50px'}}>
-        <div className="main-content">
-        <div className="text-content" style={{paddingTop : '100px'}}>
-            <h2 style={{fontSize : '40px' }} >Error 200!!<br/> Forgot Password</h2>
-            <p style={{fontSize : '25px'}} className='my-3'>Please Contact the <br/>Administrative Department</p>
-        
-        <div>
-        <Link to="/" className="hyperlink">Back to Main Page</Link>
-        </div></div>
-        <div className="image-content">
-            <img src={img}/>
+    <div style={{ height: '100vh', paddingBottom: '50px', paddingTop: '50px', backgroundColor: '#c9c9c9', fontFamily: 'Arial, sans-serif' }}>
+      <div className={styles.mainContent}>
+        <div className={styles.textContent} style={{ paddingTop: '100px' }}>
+          <h2 style={{ fontSize: '40px' }}>
+            Error 200!!<br /> Forgot Password
+          </h2>
+          <p style={{ fontSize: '25px' }} className='my-3'>
+            Please Contact the <br />Administrative Department
+          </p>
+          <div>
+            <Link to="/" className={styles.hyperlink}>Back to Main Page</Link>
+          </div>
         </div>
+        <div className={styles.imageContent}>
+          <img src={img} alt="Forgot Password" />
+        </div>
+      </div>
     </div>
-
-    </div>
-    
-  )
+  );
 }
